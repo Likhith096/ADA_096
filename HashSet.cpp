4573d996ca1568f0e7f759e40adf938190613344
Likhith096 <117436338+Likhith096@users.noreply.github.com>
2023-06-10 12:39:06 +0530

    Add files via upload

---
+++ HashSet.cpp
@@ -0,0 +1,30 @@
+class MyHashSet {
+    bool arr[1000001];
+public:
+    MyHashSet() {
+        for(int i=0; i<1e6;i++)
+        {
+            arr[i]= false;
+        }
+    }
+    
+    void add(int key) {
+        arr[key] = true;
+    }
+    
+    void remove(int key) {
+        arr[key] = false;
+    }
+    
+    bool contains(int key) {
+        return arr[key];
+    }
+};
+
+/**
+ * Your MyHashSet object will be instantiated and called as such:
+ * MyHashSet* obj = new MyHashSet();
+ * obj->add(key);
+ * obj->remove(key);
+ * bool param_3 = obj->contains(key);
+ */
